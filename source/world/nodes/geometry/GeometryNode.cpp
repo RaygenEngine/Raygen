c40b0c7f7543b8dbc2c713a236b3c0c65ebdc418
katagis <fox.dealos@gmail.com>
2019-10-20 18:31:03 +0300

    Add gltf drag to outliner

--- source/world/nodes/geometry/GeometryNode.cpp
+++ source/world/nodes/geometry/GeometryNode.cpp
@@ -1,6 +1,17 @@
 #include "world/nodes/geometry/GeometryNode.h"
 #include "asset/AssetManager.h"
 
+GeometryNode::GeometryNode()
+{
+	m_model = AssetManager::GetOrCreate<ModelPod>("/genEmptyModel");
+}
+
+void GeometryNode::SetModel(PodHandle<ModelPod> newModel)
+{
+	m_model = newModel;
+	SetDirty(DF::ModelChange);
+}
+
 void ::GeometryNode::DirtyUpdate(DirtyFlagset dirtyFlags)
 {
 	if (dirtyFlags[DF::ModelChange]) {
