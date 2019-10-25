6e22b1b171dc18b7b27ab4310ad0fc685a9fc0e7
katagis <fox.dealos@gmail.com>
2019-10-25 19:56:43 +0300

    Pilot & focus camera shortcuts

--- source/editor/Editor.h
+++ source/editor/Editor.h
@@ -125,6 +125,7 @@ public:
 	EditorCameraNode* m_editorCamera;
 	bool m_hasEditorCameraCachedMatrix{ false };
 	glm::mat4 m_editorCameraCachedMatrix{ glm::identity<glm::mat4>() };
+	glm::mat4 m_editorCameraPrePilotPos{ glm::identity<glm::mat4>() };
 
 	std::unique_ptr<NodeContextActions> m_nodeContextActions;
 
