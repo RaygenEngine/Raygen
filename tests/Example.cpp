59299232eb3259ecaea97675bd23abd00462832c
katagis <fox.dealos@gmail.com>
2020-01-27 18:15:54 +0200

    Add catch2 and test example

---
+++ tests/Example.cpp
@@ -0,0 +1,45 @@
+// Include engine pch
+#include "pch/pch.h"
+
+// Include the engine headers required for testing
+#include "system/EngineEvents.h"
+// Include any Raygen-Test specific header files (if any)
+
+// Include catch last
+#include <catch2/catch.hpp>
+
+
+TEST_CASE("Multicast Event & Listener", "core")
+{
+	MulticastEvent<int32> SimpleEvent;
+
+	REQUIRE(SimpleEvent.IsBound() == false);
+	{
+		MulticastEvent<int32>::Listener Listener{ SimpleEvent };
+		Listener.Bind([](int32 value) { REQUIRE(value == 42); });
+
+		REQUIRE(SimpleEvent.IsBound() == true);
+
+		SimpleEvent.Broadcast(42);
+	}
+	REQUIRE(SimpleEvent.IsBound() == false);
+}
+
+struct EventListenerTest {
+	DECLARE_EVENT_LISTENER(focusListener, Event::OnWindowFocus);
+
+	void OnWindowFocus(bool newFocus) { REQUIRE(newFocus == true); }
+
+	EventListenerTest() { focusListener.BindMember(this, &EventListenerTest::OnWindowFocus); }
+};
+
+TEST_CASE("Engine Event & Scoped Listener", "system")
+{
+	Event::OnWindowFocus.Broadcast(false);
+	{
+		EventListenerTest listener;
+		Event::OnWindowFocus.Broadcast(true);
+		REQUIRE(Event::OnWindowFocus.IsBound() == true);
+	}
+	REQUIRE(Event::OnWindowFocus.IsBound() == false);
+}
