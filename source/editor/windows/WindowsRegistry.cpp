b7c19692844ce9e6dc43ff5155a9ad0f3745d99e
katagis <fox.dealos@gmail.com>
2020-02-13 11:56:03 +0200

    Use central node instead of window for rendering

--- source/editor/windows/WindowsRegistry.cpp
+++ source/editor/windows/WindowsRegistry.cpp
@@ -15,46 +15,9 @@
 
 
 namespace ed {
-class ViewportWindow : public UniqueWindow {
-public:
-	ViewportWindow(const std::string& name)
-		: UniqueWindow(name)
-	{
-	}
-
-	void OnDraw(const char* title, bool* keepOpen) override
-	{
-
-		ImGuiWindowFlags flags = ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoCollapse;
-		//| ImGuiWindowFlags_NoMouseInputs;
-		auto copy = g_ViewportCoordinates;
-
-		if (ImGui::Begin(title, nullptr, flags)) {
-			auto windowRelative = glm::uvec2(ImGui::GetWindowPos().x - ImGui::GetWindowViewport()->Pos.x,
-				ImGui::GetWindowPos().y - ImGui::GetWindowViewport()->Pos.y);
-
-			g_ViewportCoordinates.position = { windowRelative.x, windowRelative.y };
-			g_ViewportCoordinates.size = { ImGui::GetWindowSize().x, ImGui::GetWindowSize().y };
-
-			if (copy != g_ViewportCoordinates) {
-				Event::OnViewportUpdated.Broadcast();
-			}
-
-			auto str = fmt::format("Pos: {}, {} Size: {} {}", windowRelative.x, windowRelative.y,
-				g_ViewportCoordinates.size.x, g_ViewportCoordinates.size.y);
-
-
-			ImGui::Text(str.c_str());
-		}
-
-		ImGui::End();
-	}
-};
-
 void RegisterWindows(ed::ComponentWindows& windowsComponent)
 {
 	windowsComponent.AddWindowEntry<AssetsWindow>("Assets");
-	windowsComponent.AddWindowEntry<ViewportWindow>("Viewport");
 
 	windowsComponent.AddWindowEntry<AboutWindow>("About");
 	windowsComponent.AddWindowEntry<HelpWindow>("Help");
