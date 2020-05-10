f62cc0a0622ffc011253f4de8d4b75533a2aa73a
JOHN MOSCHOS <ortiatha@outlook.com>
2020-05-10 16:48:05 +0300

    WIP: add additional render pass

--- source/rendering/objects/GBuffer.cpp
+++ source/rendering/objects/GBuffer.cpp
@@ -9,8 +9,11 @@
 #include "rendering/VulkanUtl.h"
 #include "rendering/Layouts.h"
 
+ConsoleVariable<int32> console_rCullMode("r.culling", static_cast<int32>(vk::CullModeFlagBits::eBack));
+
 namespace vl {
-GBuffer::GBuffer(vk::RenderPass renderPass, uint32 width, uint32 height)
+GBuffer::GBuffer(GBufferPass* passInfo, uint32 width, uint32 height)
+	: m_extent({ width, height })
 {
 	m_descSet = Layouts->gBufferDescLayout.GetDescriptorSet();
 
@@ -39,7 +42,7 @@ GBuffer::GBuffer(vk::RenderPass renderPass, uint32 width, uint32 height)
 
 	vk::FramebufferCreateInfo createInfo{};
 	createInfo
-		.setRenderPass(renderPass) //
+		.setRenderPass(passInfo->m_renderPass.get()) //
 		.setAttachmentCount(static_cast<uint32>(views.size()))
 		.setPAttachments(views.data())
 		.setWidth(width)
@@ -72,6 +75,185 @@ GBuffer::GBuffer(vk::RenderPass renderPass, uint32 width, uint32 height)
 
 		Device->updateDescriptorSets(1u, &descriptorWrite, 0u, nullptr);
 	}
+
+	MakePipeline(passInfo);
+}
+
+void GBuffer::MakePipeline(GBufferPass* passInfo)
+{
+	static auto& gpuShader = GpuAssetManager->CompileShader("engine-data/spv/gbuffer.shader");
+	gpuShader.onCompile = [&]() {
+		Device->waitIdle();
+		MakePipeline(passInfo);
+	};
+
+	if (!gpuShader.HasCompiledSuccessfully()) {
+		LOG_ERROR("Geometry Pipeline skipped due to shader compilation errors.");
+		return;
+	}
+
+	auto& fragShaderModule = gpuShader.frag;
+	auto& vertShaderModule = gpuShader.vert;
+
+	vk::PipelineShaderStageCreateInfo vertShaderStageInfo{};
+	vertShaderStageInfo
+		.setStage(vk::ShaderStageFlagBits::eVertex) //
+		.setModule(*vertShaderModule)
+		.setPName("main");
+	// fixed-function stage
+	vk::PipelineVertexInputStateCreateInfo vertexInputInfo{};
+
+	vk::VertexInputBindingDescription bindingDescription{};
+	bindingDescription
+		.setBinding(0u) //
+		.setStride(sizeof(Vertex))
+		.setInputRate(vk::VertexInputRate::eVertex);
+
+	std::array<vk::VertexInputAttributeDescription, 5> attributeDescriptions{};
+
+	attributeDescriptions[0].binding = 0u;
+	attributeDescriptions[0].location = 0u;
+	attributeDescriptions[0].format = vk::Format::eR32G32B32Sfloat;
+	attributeDescriptions[0].offset = offsetof(Vertex, position);
+
+	attributeDescriptions[1].binding = 0u;
+	attributeDescriptions[1].location = 1u;
+	attributeDescriptions[1].format = vk::Format::eR32G32B32Sfloat;
+	attributeDescriptions[1].offset = offsetof(Vertex, normal);
+
+	attributeDescriptions[2].binding = 0u;
+	attributeDescriptions[2].location = 2u;
+	attributeDescriptions[2].format = vk::Format::eR32G32B32Sfloat;
+	attributeDescriptions[2].offset = offsetof(Vertex, tangent);
+
+	attributeDescriptions[3].binding = 0u;
+	attributeDescriptions[3].location = 3u;
+	attributeDescriptions[3].format = vk::Format::eR32G32B32Sfloat;
+	attributeDescriptions[3].offset = offsetof(Vertex, bitangent);
+
+	attributeDescriptions[4].binding = 0u;
+	attributeDescriptions[4].location = 4u;
+	attributeDescriptions[4].format = vk::Format::eR32G32Sfloat;
+	attributeDescriptions[4].offset = offsetof(Vertex, uv);
+
+	vertexInputInfo
+		.setVertexBindingDescriptionCount(1u) //
+		.setVertexAttributeDescriptionCount(static_cast<uint32_t>(attributeDescriptions.size()))
+		.setPVertexBindingDescriptions(&bindingDescription)
+		.setPVertexAttributeDescriptions(attributeDescriptions.data());
+
+	vk::PipelineShaderStageCreateInfo fragShaderStageInfo{};
+	fragShaderStageInfo
+		.setStage(vk::ShaderStageFlagBits::eFragment) //
+		.setModule(*fragShaderModule)
+		.setPName("main");
+
+	std::array shaderStages = { vertShaderStageInfo, fragShaderStageInfo };
+
+
+	vk::PipelineInputAssemblyStateCreateInfo inputAssembly{};
+	inputAssembly
+		.setTopology(vk::PrimitiveTopology::eTriangleList) //
+		.setPrimitiveRestartEnable(VK_FALSE);
+
+	vk::Rect2D scissor{};
+	scissor
+		.setOffset({ 0, 0 }) //
+		.setExtent(m_extent);
+
+	vk::Viewport viewport{};
+	viewport
+		.setX(0) //
+		.setY(0)
+		.setWidth(static_cast<float>(m_extent.width))
+		.setHeight(static_cast<float>(m_extent.height))
+		.setMinDepth(0.f)
+		.setMaxDepth(1.f);
+
+	vk::PipelineViewportStateCreateInfo viewportState{};
+	viewportState
+		.setViewportCount(1u) //
+		.setPViewports(&viewport)
+		.setScissorCount(1u)
+		.setPScissors(&scissor);
+
+	vk::PipelineRasterizationStateCreateInfo rasterizer{};
+	rasterizer
+		.setDepthClampEnable(VK_FALSE) //
+		.setRasterizerDiscardEnable(VK_FALSE)
+		.setPolygonMode(vk::PolygonMode::eFill)
+		.setLineWidth(1.f)
+		.setCullMode(static_cast<vk::CullModeFlags>(console_rCullMode.Get()))
+		.setFrontFace(vk::FrontFace::eClockwise)
+		.setDepthBiasEnable(VK_FALSE)
+		.setDepthBiasConstantFactor(0.f)
+		.setDepthBiasClamp(0.f)
+		.setDepthBiasSlopeFactor(0.f);
+
+	vk::PipelineMultisampleStateCreateInfo multisampling{};
+	multisampling
+		.setSampleShadingEnable(VK_FALSE) //
+		.setRasterizationSamples(vk::SampleCountFlagBits::e1)
+		.setMinSampleShading(1.f)
+		.setPSampleMask(nullptr)
+		.setAlphaToCoverageEnable(VK_FALSE)
+		.setAlphaToOneEnable(VK_FALSE);
+
+	std::array<vk::PipelineColorBlendAttachmentState, 5> colorBlendAttachment{};
+	for (uint32 i = 0u; i < 5; ++i) {
+		colorBlendAttachment[i]
+			.setColorWriteMask(vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG
+							   | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA) //
+			.setBlendEnable(VK_FALSE)
+			.setSrcColorBlendFactor(vk::BlendFactor::eOne)
+			.setDstColorBlendFactor(vk::BlendFactor::eZero)
+			.setColorBlendOp(vk::BlendOp::eAdd)
+			.setSrcAlphaBlendFactor(vk::BlendFactor::eOne)
+			.setDstAlphaBlendFactor(vk::BlendFactor::eZero)
+			.setAlphaBlendOp(vk::BlendOp::eAdd);
+	}
+
+	vk::PipelineColorBlendStateCreateInfo colorBlending{};
+	colorBlending
+		.setLogicOpEnable(VK_FALSE) //
+		.setLogicOp(vk::LogicOp::eCopy)
+		.setAttachmentCount(static_cast<uint32>(colorBlendAttachment.size()))
+		.setPAttachments(colorBlendAttachment.data())
+		.setBlendConstants({ 0.f, 0.f, 0.f, 0.f });
+
+
+	// depth and stencil state
+	vk::PipelineDepthStencilStateCreateInfo depthStencil{};
+	depthStencil
+		.setDepthTestEnable(VK_TRUE) //
+		.setDepthWriteEnable(VK_TRUE)
+		.setDepthCompareOp(vk::CompareOp::eLess)
+		.setDepthBoundsTestEnable(VK_FALSE)
+		.setMinDepthBounds(0.0f) // Optional
+		.setMaxDepthBounds(1.0f) // Optional
+		.setStencilTestEnable(VK_FALSE)
+		.setFront({}) // Optional
+		.setBack({}); // Optional
+
+	vk::GraphicsPipelineCreateInfo pipelineInfo{};
+	pipelineInfo
+		.setStageCount(static_cast<uint32>(shaderStages.size())) //
+		.setPStages(shaderStages.data())
+		.setPVertexInputState(&vertexInputInfo)
+		.setPInputAssemblyState(&inputAssembly)
+		.setPViewportState(&viewportState)
+		.setPRasterizationState(&rasterizer)
+		.setPMultisampleState(&multisampling)
+		.setPDepthStencilState(&depthStencil)
+		.setPColorBlendState(&colorBlending)
+		.setPDynamicState(nullptr)
+		.setLayout(passInfo->m_pipelineLayout.get())
+		.setRenderPass(passInfo->m_renderPass.get())
+		.setSubpass(0u)
+		.setBasePipelineHandle({})
+		.setBasePipelineIndex(-1);
+
+	m_pipeline = Device->createGraphicsPipelineUnique(nullptr, pipelineInfo);
 }
 
 void GBuffer::TransitionForWrite(vk::CommandBuffer* cmdBuffer)
