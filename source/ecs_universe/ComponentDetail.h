7ea3d54db43293d89727ed7d608f17a89c776676
katagis <fox.dealos@gmail.com>
2020-08-03 16:55:10 +0300

    Add scene cmd system

--- source/ecs_universe/ComponentDetail.h
+++ source/ecs_universe/ComponentDetail.h
@@ -3,31 +3,60 @@
 struct SceneCompBase;
 
 namespace componentdetail {
-template<typename T, typename = void>
-struct HasDirtySubstruct : std::false_type {
-};
+namespace impl {
+	template<typename T, typename = void>
+	struct HasDirtySubstruct : std::false_type {
+	};
 
+	template<typename T>
+	struct HasDirtySubstruct<T, std::enable_if_t<std::is_empty_v<typename T::Dirty>>> : std::true_type {
+	};
+
+
+	template<typename T, typename = void>
+	struct HasCreateDestroySubstructs : std::false_type {
+	};
+
+	template<typename T>
+	struct HasCreateDestroySubstructs<T,
+		std::enable_if_t<std::conjunction_v<std::is_empty<typename T::Create>, std::is_empty<typename T::Destroy>>>>
+		: std::true_type {
+	};
+
+
+	template<typename T, typename = void>
+	struct HasSceneType : std::false_type {
+	};
+
+	template<typename T>
+	struct HasSceneType<T, std::void_t<typename T::RenderSceneType>> : std::true_type {
+	};
+} // namespace impl
+
+
+// Detect if the struct T contains an inner empty struct Dirty
 template<typename T>
-struct HasDirtySubstruct<T, std::enable_if_t<std::is_empty_v<typename T::Dirty>>> : std::true_type {
-};
+constexpr bool HasDirtySubstructV = impl::HasDirtySubstruct<T>::value;
 
+// Detect if the struct T contains the inner empty structs Create and Destroy
 template<typename T>
-constexpr bool HasDirtySubstructV = HasDirtySubstruct<T>::value;
+constexpr bool HasCreateDestorySubstructsV = impl::HasCreateDestroySubstructs<T>::value;
 
-template<typename T, typename = void>
-struct HasCreateDestroySubstructs : std::false_type {
-};
-
+// Detect if the struct T contains RenderSceneType as type definition. (Detect if the component is declared as a scene
+// comp)
 template<typename T>
-struct HasCreateDestroySubstructs<T,
-	std::enable_if_t<std::conjunction_v<std::is_empty<typename T::Create>, std::is_empty<typename T::Destroy>>>>
-	: std::true_type {
-};
+constexpr bool HasSceneTypeV = impl::HasSceneType<T>::value;
 
+
+//
 template<typename T>
-constexpr bool HasCreateDestorySubstructsV = HasCreateDestroySubstructs<T>::value;
+constexpr bool IsSceneComponent = HasSceneTypeV<T>&& HasDirtySubstructV<T>&& HasCreateDestorySubstructsV<T>;
+
 
 } // namespace componentdetail
 
 template<typename T>
 concept CComponent = true;
+
+template<typename T>
+concept CSceneComp = CComponent<T>&& componentdetail::IsSceneComponent<T>;
---
+++ source/ecs_universe/systems/SceneCmdSystem.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "SceneCmdSystem.h"
+
+
+void SceneCmdSystem::WriteSceneCmds(Scene_* scene, entt::registry& registry)
+{
+	// Write scene creations
+	for (auto fn : Get().m_createCmds) {
+		fn(scene, registry);
+	}
+
+	// Write scene destructions
+	for (auto fn : Get().m_destroyCmds) {
+		fn(scene, registry);
+	}
+
+	// Write scene dirty updates
+	for (auto fn : Get().m_dirtyCmds) {
+		fn(scene, registry);
+	}
+
+	// Write scene transform updates
+	for (auto fn : Get().m_transformCmds) {
+		fn(scene, registry);
+	}
+}
