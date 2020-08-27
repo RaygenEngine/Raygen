fdcd7ecf1fa5a45c45f048668f50b3cdc1d6ac00
katagis <fox.dealos@gmail.com>
2020-08-27 22:36:37 +0300

    Use functions for OutPass pipeline creation

--- source/rendering/Renderer.cpp
+++ source/rendering/Renderer.cpp
@@ -25,6 +25,7 @@
 #include "rendering/scene/SceneSpotlight.h"
 #include "rendering/structures/Depthmap.h"
 #include "rendering/wrappers/Swapchain.h"
+#include "rendering/core/PipeUtl.h"
 
 #include <editor/imgui/ImguiImpl.h>
 
@@ -86,123 +87,8 @@ void Renderer_::MakeCopyHdrPipeline(vk::RenderPass outRp)
 		MakeCopyHdrPipeline(outRp);
 	};
 
-	std::vector shaderStages = gpuShader.shaderStages;
-
-	// fixed-function stage
-	vk::PipelineVertexInputStateCreateInfo vertexInputInfo{};
-
-	vk::PipelineInputAssemblyStateCreateInfo inputAssembly{};
-	inputAssembly
-		.setTopology(vk::PrimitiveTopology::eTriangleList) //
-		.setPrimitiveRestartEnable(VK_FALSE);
-
-	// those are dynamic so they will be updated when needed
-	vk::Viewport viewport{};
-	vk::Rect2D scissor{};
-
-	vk::PipelineViewportStateCreateInfo viewportState{};
-	viewportState
-		.setViewportCount(1u) //
-		.setPViewports(&viewport)
-		.setScissorCount(1u)
-		.setPScissors(&scissor);
-
-	vk::PipelineRasterizationStateCreateInfo rasterizer{};
-	rasterizer
-		.setDepthClampEnable(VK_FALSE) //
-		.setRasterizerDiscardEnable(VK_FALSE)
-		.setPolygonMode(vk::PolygonMode::eFill)
-		.setLineWidth(1.f)
-		.setCullMode(vk::CullModeFlagBits::eBack)
-		.setFrontFace(vk::FrontFace::eClockwise)
-		.setDepthBiasEnable(VK_FALSE)
-		.setDepthBiasConstantFactor(0.f)
-		.setDepthBiasClamp(0.f)
-		.setDepthBiasSlopeFactor(0.f);
-
-	vk::PipelineMultisampleStateCreateInfo multisampling{};
-	multisampling
-		.setSampleShadingEnable(VK_FALSE) //
-		.setRasterizationSamples(vk::SampleCountFlagBits::e1)
-		.setMinSampleShading(1.f)
-		.setPSampleMask(nullptr)
-		.setAlphaToCoverageEnable(VK_FALSE)
-		.setAlphaToOneEnable(VK_FALSE);
-
-	vk::PipelineColorBlendAttachmentState colorBlendAttachment{};
-	colorBlendAttachment
-		.setColorWriteMask(vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG
-						   | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA) //
-		.setBlendEnable(VK_TRUE)
-		.setSrcColorBlendFactor(vk::BlendFactor::eOne)
-		.setDstColorBlendFactor(vk::BlendFactor::eOne)
-		.setColorBlendOp(vk::BlendOp::eAdd)
-		.setSrcAlphaBlendFactor(vk::BlendFactor::eOne)
-		.setDstAlphaBlendFactor(vk::BlendFactor::eOne)
-		.setAlphaBlendOp(vk::BlendOp::eAdd);
-
-	vk::PipelineColorBlendStateCreateInfo colorBlending{};
-	colorBlending
-		.setLogicOpEnable(VK_FALSE) //
-		.setLogicOp(vk::LogicOp::eCopy)
-		.setAttachmentCount(1u)
-		.setPAttachments(&colorBlendAttachment)
-		.setBlendConstants({ 0.f, 0.f, 0.f, 0.f });
-
-
-	// dynamic states
-	vk::DynamicState dynamicStates[] = { vk::DynamicState::eViewport, vk::DynamicState::eScissor };
-
-	vk::PipelineDynamicStateCreateInfo dynamicStateInfo{};
-	dynamicStateInfo
-		.setDynamicStateCount(2u) //
-		.setPDynamicStates(dynamicStates);
-
-	std::array layouts = { Layouts->singleSamplerDescLayout.setLayout.get() };
-
-	// pipeline layout
-	vk::PipelineLayoutCreateInfo pipelineLayoutInfo{};
-	pipelineLayoutInfo
-		.setSetLayoutCount(static_cast<uint32>(layouts.size())) //
-		.setPSetLayouts(layouts.data())
-		.setPushConstantRangeCount(0u)
-		.setPPushConstantRanges(nullptr);
-
-	m_pipelineLayout = Device->createPipelineLayoutUnique(pipelineLayoutInfo);
-
-	// depth and stencil state
-	vk::PipelineDepthStencilStateCreateInfo depthStencil{};
-	depthStencil
-		.setDepthTestEnable(VK_FALSE) //
-		.setDepthWriteEnable(VK_FALSE)
-		.setDepthCompareOp(vk::CompareOp::eLess)
-		.setDepthBoundsTestEnable(VK_FALSE)
-		.setMinDepthBounds(0.0f) // Optional
-		.setMaxDepthBounds(1.0f) // Optional
-		.setStencilTestEnable(VK_FALSE)
-		.setFront({}) // Optional
-		.setBack({}); // Optional
-
-	vk::GraphicsPipelineCreateInfo pipelineInfo{};
-	pipelineInfo
-		.setStageCount(static_cast<uint32>(shaderStages.size())) //
-		.setPStages(shaderStages.data())
-		.setPVertexInputState(&vertexInputInfo)
-		.setPInputAssemblyState(&inputAssembly)
-		.setPViewportState(&viewportState)
-		.setPRasterizationState(&rasterizer)
-		.setPMultisampleState(&multisampling)
-		.setPDepthStencilState(&depthStencil)
-		.setPColorBlendState(&colorBlending)
-		.setPDynamicState(&dynamicStateInfo)
-		.setLayout(m_pipelineLayout.get())
-		// WIP: decouple
-		.setRenderPass(outRp)
-		.setSubpass(0u)
-		.setBasePipelineHandle({})
-		.setBasePipelineIndex(-1);
-
-	m_pipeline = Device->createGraphicsPipelineUnique(nullptr, pipelineInfo);
+	m_pipelineLayout = rvk::makeLayoutNoPC({ Layouts->singleSamplerDescLayout.setLayout.get() });
+	m_pipeline = rvk::makePostProcPipeline(gpuShader.shaderStages, *m_pipelineLayout, outRp);
 }
 
 void Renderer_::MakeRtPipeline()
