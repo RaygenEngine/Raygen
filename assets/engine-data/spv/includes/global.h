cbd6de23dd0476d7e70662bd67aaea46ad61c4d9
Renoras <ortiatha@yahoo.gr>
2020-08-27 15:55:37 +0300

    WIP Tidy shaders

--- assets/engine-data/spv/includes/global.h
+++ assets/engine-data/spv/includes/global.h
@@ -19,18 +19,9 @@ vec3 saturate(vec3 v)
     return clamp(v, vec3(0.0), vec3(1.0));
 }
 
-vec4 SampleCubemapLH(samplerCube cubemap, vec3 RHdirection) {
-	return texture(cubemap, vec3(RHdirection.x, RHdirection.y, -RHdirection.z));
-}
-
-void createOnb(const vec3 normal, inout vec3 T, inout vec3 B, inout vec3 N)
+vec4 SampleCubemapLH(samplerCube cubemap, vec3 RHdirection) 
 {
-	N = normalize(normal);
-	T = cross(N, vec3(0.0f, 1.0f, 0.0f));
-	if(abs(T.x) < 0.001f && abs(T.y) < 0.001f && abs(T.z) < 0.001f)
-		T = cross(N, vec3(1.0f, 0.0f, 0.0f));
-	T = normalize(T);
-	B = cross(N, T);
+	return texture(cubemap, vec3(RHdirection.x, RHdirection.y, -RHdirection.z));
 }
 
 #endif
