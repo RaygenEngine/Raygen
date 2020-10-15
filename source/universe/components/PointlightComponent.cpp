8cabea8ade501f0ab20e09d0647c3dc382dfd962
JOHN MOSCHOS <ortiatha@outlook.com>
2020-10-15 16:15:37 +0300

    Add pointlight volumes

--- source/universe/components/PointlightComponent.cpp
+++ source/universe/components/PointlightComponent.cpp
@@ -4,8 +4,18 @@
 
 DECLARE_DIRTY_FUNC(CPointlight)(BasicComponent& bc)
 {
-	return [=](ScenePointlight& pl) {
-		pl.ubo.position = glm::vec4(bc.world().position, 1.f);
+	auto lightP = color * intensity;
+	float lightMax = std::fmaxf(std::fmaxf(lightP.x, lightP.y), lightP.z);
+	float radius
+		= (-linearTerm
+			  + std::sqrtf(linearTerm * linearTerm - 4.f * quadraticTerm * (constantTerm - (256.f / 5.f) * lightMax)))
+		  / (2.f * quadraticTerm);
+
+	glm::mat4 volumeTransform = math::transformMat({ radius, radius, radius }, {}, bc.world().position);
+
+	return [=, position = bc.world().position](ScenePointlight& pl) {
+		pl.ubo.position = glm::vec4(position, 1.f);
+		pl.volumeTransform = volumeTransform;
 
 		if constexpr (FullDirty) {
 			pl.ubo.color = glm::vec4(color, 1.f);
