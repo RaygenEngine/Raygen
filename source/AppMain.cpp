e0699a7c4dfb109d1f7358304db16328df3b3b7a
katagis <fox.dealos@gmail.com>
2020-11-06 22:25:07 +0200

    Add bilinear irradiance grid sampling

--- source/AppMain.cpp
+++ source/AppMain.cpp
@@ -7,7 +7,7 @@ int32 main(int32 argc, char* argv[])
 	// Init logger (global access, not engine, app or window bound)
 	Log.Init(LogLevel::Info);
 
-	App app;
+	App_ app;
 	app.PreMainInit(argc, argv);
 	return app.Main(argc, argv);
 }
