9473367672ba5b1e28fe3ff02960eda9e76ec038
JOHN MOSCHOS <ortiatha@outlook.com>
2020-11-29 00:55:04 +0200

    Revert "Revert "Tidy ViewImage constructors""
    This reverts commit 6217989389bee7c028823313edcb2d7f3f2e6c02.

--- source/rendering/Renderer.cpp
+++ source/rendering/Renderer.cpp
@@ -169,7 +169,7 @@ void Renderer_::RecordSecondaryPasses(vk::CommandBuffer cmdBuffer, const SceneRe
 	// m_mirorPass.RecordPass(cmdBuffer, sceneDesc);
 
 	// WIP: decide, also can it be merged with mirror pass?
-	// m_indirectSpecPass.RecordPass(cmdBuffer, sceneDesc);
+	m_indirectSpecPass.RecordPass(cmdBuffer, sceneDesc);
 }
 
 void Renderer_::RecordPostProcessPasses(vk::CommandBuffer cmdBuffer, const SceneRenderDesc& sceneDesc)
